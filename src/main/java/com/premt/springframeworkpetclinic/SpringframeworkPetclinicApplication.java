5ab284bfe8e7b4125440d5f1ba2531a36a858aaa
premt <premtim.farizi@gmail.com>
2019-12-06 02:45:47 +0100

    Initial App Commit

---
+++ src/main/java/com/premt/springframeworkpetclinic/SpringframeworkPetclinicApplication.java
@@ -0,0 +1,13 @@
+package com.premt.springframeworkpetclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringframeworkPetclinicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringframeworkPetclinicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/premt/springframeworkpetclinic/SpringframeworkPetclinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.premt.springframeworkpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringframeworkPetclinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
