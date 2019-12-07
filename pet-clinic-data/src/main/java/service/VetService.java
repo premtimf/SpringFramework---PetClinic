73368cb65e0e4aa3ef41ccc0e70f6f7524d8d5ab
premt <premtim.farizi@gmail.com>
2019-12-07 05:27:03 +0100

    Refactored Service Interfaces using CrudBase Interface. close #10

--- pet-clinic-data/src/main/java/service/VetService.java
+++ pet-clinic-data/src/main/java/service/VetService.java
@@ -2,11 +2,6 @@ package service;
 
 import com.premt.springframeworkpetclinic.model.Vet;
 
-import java.util.Set;
+public interface VetService extends CrudService<Vet, Long>{
 
-public interface VetService {
-
-    Vet findById (Long id);
-    Vet save(Vet vet);
-    Set<Vet> findAll();
 }
