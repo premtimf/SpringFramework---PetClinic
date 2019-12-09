c51003ba3db0dd54ed2a3a755414d24dbf2f170b
premt <premtim.farizi@gmail.com>
2019-12-09 20:00:06 +0100

    added the list of owners at owners/index

--- pet-clinic-web/src/main/java/com/premt/springframeworkpetclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/com/premt/springframeworkpetclinic/controllers/IndexController.java
@@ -1,12 +1,11 @@
 package com.premt.springframeworkpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
-    @RequestMapping({"" , "/" , "index" , "index.html"})
+//    @RequestMapping({"" , "/" , "index" , "index.html"})
     public String getIndex(){
 
         return "index";
