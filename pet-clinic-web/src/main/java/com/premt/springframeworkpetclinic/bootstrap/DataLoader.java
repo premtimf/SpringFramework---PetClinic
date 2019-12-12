7364d29aba4e948458117e1e6df1b5bc0a9159dc
premt <premtim.farizi@gmail.com>
2019-12-12 13:48:37 +0100

    Added two PetTypes with bootstrap datastarter. Closes #24.

--- pet-clinic-web/src/main/java/com/premt/springframeworkpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/com/premt/springframeworkpetclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package com.premt.springframeworkpetclinic.bootstrap;
 
 import com.premt.springframeworkpetclinic.model.Owner;
+import com.premt.springframeworkpetclinic.model.PetType;
 import com.premt.springframeworkpetclinic.model.Vet;
 import com.premt.springframeworkpetclinic.service.OwnerService;
+import com.premt.springframeworkpetclinic.service.PetTypeService;
 import com.premt.springframeworkpetclinic.service.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,14 +14,27 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("Dog");
+
+        petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        cat.setName("Cat");
+
+        petTypeService.save(cat);
+
         Owner owner1 =  new Owner();
         owner1.setFirstName("Premtim");
         owner1.setLastName("Farizi");
