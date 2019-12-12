95d9f36bb9cd3885b5c8daeb3e9b6059d56bcd0d
premt <premtim.farizi@gmail.com>
2019-12-12 23:38:33 +0100

    Added SpecialtyService Map. Closes #23.

---
+++ pet-clinic-data/src/main/java/com/premt/springframeworkpetclinic/service/SpecialtyService.java
@@ -0,0 +1,6 @@
+package com.premt.springframeworkpetclinic.service;
+
+import com.premt.springframeworkpetclinic.model.Specialty;
+
+public interface SpecialtyService extends CrudService<Specialty, Long> {
+}
