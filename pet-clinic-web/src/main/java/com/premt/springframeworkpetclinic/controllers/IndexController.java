63c29fe4388ec9c8e7a775ad6d1a6d475b2c498a
premt <premtim.farizi@gmail.com>
2019-12-14 11:51:25 +0100

    Fixed broken links in the menu. Closes #37.

--- pet-clinic-web/src/main/java/com/premt/springframeworkpetclinic/controllers/IndexController.java
+++ pet-clinic-web/src/main/java/com/premt/springframeworkpetclinic/controllers/IndexController.java
@@ -1,13 +1,20 @@
 package com.premt.springframeworkpetclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class IndexController {
 
-//    @RequestMapping({"" , "/" , "index" , "index.html"})
+    @RequestMapping({"" , "/" , "index" , "index.html"})
     public String getIndex(){
 
         return "index";
+    }
+
+    @RequestMapping("/oups")
+    public String oups(Model model){
+        return "notImpl";
     }
 }
